856a11ea304810a554aaa2818919f98d275ce76a
Chirag Rupani <chiragrupani2004@gmail.com>
2022-01-15 19:04:02 +1100

    Added InxededDB

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -4,6 +4,7 @@ import { useQuery } from "@apollo/client";
 
 import { QUERY_PRODUCTS } from "../utils/queries";
 import { useStoreContext } from "../utils/GlobalState";
+import { idbPromise } from "../utils/helpers";
 import {
     REMOVE_FROM_CART,
     UPDATE_CART_QUANTITY,
@@ -32,8 +33,21 @@ function Detail() {
                 type: UPDATE_PRODUCTS,
                 products: data.products,
             });
+
+            data.products.forEach((product) => {
+                idbPromise("products", "put", product);
+            });
         }
-    }, [products, data, dispatch, id]);
+        // get cache from idb
+        else if (!loading) {
+            idbPromise("products", "get").then((indexedProducts) => {
+                dispatch({
+                    type: UPDATE_PRODUCTS,
+                    products: indexedProducts,
+                });
+            });
+        }
+    }, [products, data, loading, dispatch, id]);
 
     const addToCart = () => {
         const itemInCart = cart.find((cartItem) => cartItem._id === id);
@@ -44,10 +58,22 @@ function Detail() {
                 _id: id,
                 purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
             });
+
+            // if we're updating quantity, use existing item data and increment purchaseQuantity value by one
+            idbPromise("cart", "put", {
+                ...itemInCart,
+                purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
+            });
         } else {
             dispatch({
                 type: ADD_TO_CART,
                 product: { ...currentProduct, purchaseQuantity: 1 },
+            });
+
+            // if product isn't in the cart yet, add it to the current shopping cart in IndexedDB
+            idbPromise("cart", "put", {
+                ...currentProduct,
+                purchaseQuantity: 1,
             });
         }
     };
@@ -57,6 +83,9 @@ function Detail() {
             type: REMOVE_FROM_CART,
             _id: currentProduct._id,
         });
+
+        // upon removal from cart, delete the item from IndexedDB using the `currentProduct._id` to locate what to remove
+        idbPromise("cart", "delete", { ...currentProduct });
     };
 
     return (
